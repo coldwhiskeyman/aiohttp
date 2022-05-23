cd59c6b4d7e2c28c88b95bfdc22873adac21364f
coldwhiskeyman <eldiablo5783@gmail.com>
2022-05-23 10:00:04 +0300

    Initial commit

---
+++ aiohttpdemo_polls/main.py
@@ -0,0 +1,17 @@
+import aiohttp_jinja2
+import jinja2
+from aiohttp import web
+
+from settings import config, BASE_DIR
+from routes import setup_routes
+from db import pg_context
+from middleware import setup_middlewares
+
+app = web.Application()
+app['config'] = config
+aiohttp_jinja2.setup(
+    app, loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttpdemo_polls' / 'templates')))
+setup_routes(app)
+setup_middlewares(app)
+app.cleanup_ctx.append(pg_context)
+web.run_app(app)
---
+++ aiohttpdemo_polls/views.py
@@ -0,0 +1,13 @@
+import aiohttp_jinja2
+from aiohttp import web
+
+import db
+
+
+@aiohttp_jinja2.template('index.html')
+async def index(request):
+    async with request.app['db'].acquire() as conn:
+        cursor = await conn.execute(db.question.select())
+        records = await cursor.fetchall()
+        questions = [dict(q) for q in records]
+        return {"questions": questions}
